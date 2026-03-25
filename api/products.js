86054448754cb7c8f5e12a1b489a5ed9fe6a08ea
KingEliPC <ittooelijah0605@gmail.com>
2026-03-25 20:58:22 +1100

    Update product data structure in API handler

--- api/products.js
+++ api/products.js
@@ -1,19 +1,15 @@
 export default function handler(req, res) {
   const products = [
-    {
-      id: 1,
-      name: "Beach Time!",
-      price: 0,
-      stock: 10,
-      description: "A summer-themed blind bag featuring Cool, Amazing, and ULTRA Cool surprises.",
-      rarityLevels: ["Cool", "Amazing", "ULTRA Cool"],
-      creators: "E‑Pix® and Liza Wuol® 2026",
-      images: [
-        "/beach-time-front.jpg",
-        "/beach-time-back.jpg"
-      ]
-    }
-  ];
+    [
+  {
+    "id": "beach-time-blind-bag",
+    "name": "Beach Time Blind Bag",
+    "description": "A fun mystery blind bag with beach-themed surprises.",
+    "price": 5.99,
+    "image": "/images/beach-time-front.jpg"
+  }
+]
 
+  ];
   res.status(200).json(products);
 }
