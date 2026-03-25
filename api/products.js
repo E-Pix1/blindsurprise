02fe29b6660dfa81384f50bc38426af46b9dae56
KingEliPC <ittooelijah0605@gmail.com>
2026-03-25 20:14:16 +1100

    Add API handler for product listings

---
+++ api/products.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  const products = [
+    {
+      id: 1,
+      name: "Beach Time!",
+      price: 0,
+      stock: 10,
+      description: "A summer-themed blind bag featuring Cool, Amazing, and ULTRA Cool surprises.",
+      rarityLevels: ["Cool", "Amazing", "ULTRA Cool"],
+      creators: "E‑Pix® and Liza Wuol® 2026",
+      images: ["beach-time-front.jpg"]
+    }
+  ];
+
+  res.status(200).json(products);
+}
