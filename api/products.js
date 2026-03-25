b53525db9cfc2a7dbf3cc62c8ed37884e918373b
KingEliPC <ittooelijah0605@gmail.com>
2026-03-25 21:08:58 +1100

    Update file path for products.json

--- api/products.js
+++ api/products.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "products.json");
+  const filePath = path.join(process.cwd(), "api", "json", "products.json");
   const fileData = fs.readFileSync(filePath, "utf8");
   const products = JSON.parse(fileData);
 
